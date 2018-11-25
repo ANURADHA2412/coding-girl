853dd1f7e804fb992005354783e4eb02585db418
ANURADHA2412 <45077926+ANURADHA2412@users.noreply.github.com>
2018-11-25 10:58:16 +0530

    Create value of ram.c

---
+++ value of ram.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+     int n, dis;
+     printf("enter the value io ram entering");
+     scanf("%d ,&n);
+ if(n==1)
+        dis=10/100 *450;
+ else
+       if =2/100 *450.;
+ else
+      if(n>2 && n<0)
+      dis= 5/100 *450;
+      printf ("the discount given to ram is %d",dis);
+      return 0;
+  }
