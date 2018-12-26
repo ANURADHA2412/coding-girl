ef063c2996000e835d963d3d4cb2e4049d3d7afb
ANURADHA2412 <45077926+ANURADHA2412@users.noreply.github.com>
2018-12-26 12:30:56 +0530

    Create passing function.c

---
+++ passing function.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int calsum (int x,int y,int z)
+int main()
+{ 
+   int a,b,c,sum;
+   printf("enter any three numbers");
+   scanf("%d %d %d",&a,&b,&c);
+   sum = ("sum = %d\n",sum);
+   return 0;
+}
+    int calsum (int x,int y,int z)
+    {
+      int d;
+      d = x+y+z;
+      return (d);
+  }
+  
